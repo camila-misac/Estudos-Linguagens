5ea48ceccf97cc03f3da43abd0ef7393eb4b833b
Camila Moura Isac <dev.camilamouraisac@gmail.com>
2025-03-02 21:05:13 +0000

    Modificação no codigo
    Adicionando mais funcionalidade ao meu jogo do número secreto.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,25 @@
 //Baixei a extenssão (para ser um meu servidor local)
 
 alert('Boas vindas ao jogo do número secreto'); //Serve para mostrar uma mensagem ao usuario. O ";" não é obrigátorio ultilizar o ";"
-let numeroSecreto = 2; //let refere-se a variavel (no caso chamo a variavel "let" e logo apos ela passo o nome da variavel e depois do = passo um valor)
+let numeroSecreto = 5; //let refere-se a variavel (no caso chamo a variavel "let" e logo apos ela passo o nome da variavel e depois do = passo um valor)
 console.log(numeroSecreto); //Usa o console.log para ver internamente atraves do log da pagina se as funções estão funcionando corretamente ou não
-let chute = prompt('Escolha um numero entre 1 e 30'); //Recebe um valor digitado pelo usuario
+let chute;
+let tentativas = 1; //Icremento de tentativas
 
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`); //Para concactenar é necessario usar ` (acentuação) ao invés de aspas simples ou duplas
+// Enquanto o chute não for igual ao numero secreto faça...
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um numero entre 1 e 30'); //Recebe um valor digitado pelo usuario
+
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); //Para concactenar é necessario usar ` (acentuação) ao invés de aspas simples ou duplas
+    }
+    else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        else {
+            alert(`O número secreto é maior que ${chute}`);            
+        }
+        tentativas++;    
+    }
 }
-else {
-    alert('Você errou!')
-}
-
-
